ab2b022c6c518f0e3d2492e7e9e2a14ab32cf854
Priya J <priyadj2001@gmai.com>
2023-04-16 08:25:17 +0530

    Initial Commit

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,14 +1,50 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppointmentScheduler.Models;
+using Azure.Messaging;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Numerics;
+using System.Security.Principal;
 
 namespace AppointmentScheduler.Controllers
 {
     public class LoginController : Controller
     {
+        IConfiguration _configuration;
+        SqlConnection _Connection;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _Connection = new SqlConnection(_configuration.GetConnectionString("LogIn"));
+        }
+
+        public List<LoginModel> GetUsers()
+        {
+            List<LoginModel> allUsers = new();
+            _Connection.Open();
+            SqlCommand cmd = new SqlCommand("Fetch_Users", _Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                LoginModel login_user = new();
+                login_user.Id = (int)dr[0];
+                login_user.UserName = (string)dr[1];
+                login_user.Email= (string) (dr[2]);
+                login_user.ContactNo = Convert.ToInt64(dr[3]);
+                login_user.Cmpy_name = dr.GetString(5);
+                login_user.Password = dr.GetString(4);
+                allUsers.Add(login_user);
+            }
+            dr.Close();
+            _Connection.Close();
+            return allUsers;
+        }
         // GET: AdminController
         public ActionResult Index()
         {
-            return View();
+            return View(GetUsers());
         }
 
         // GET: AdminController/Details/5
@@ -16,21 +52,99 @@ namespace AppointmentScheduler.Controllers
         {
             return View();
         }
-
+        //For SignUp Page
         // GET: AdminController/Create
-        public ActionResult Create()
+        public ActionResult SignUp()
         {
             return View();
+        }
+
+        void CreateUser(LoginModel login_user)
+        {
+            _Connection.Open();
+            SqlCommand cmd = new SqlCommand("Insert_User", _Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@User_name", login_user.UserName);
+            cmd.Parameters.AddWithValue("@Email", login_user.Email);
+            cmd.Parameters.AddWithValue("@ContactNo", login_user.ContactNo);
+            cmd.Parameters.AddWithValue("@Password", login_user.Password);
+            cmd.Parameters.AddWithValue("@Company_Name", login_user.Cmpy_name);
+
+            cmd.ExecuteNonQuery();
+            _Connection.Close();
         }
 
         // POST: AdminController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult SignUp(LoginModel login)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                CreateUser(login);
+                return RedirectToAction(nameof(Login));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"We have faced some issues {e}");
+                return View();
+            }
+        }
+
+        //Login Page
+        // GET: AdminController/Create
+        public ActionResult Login()
+        {
+            return View();
+        }
+        bool LoginValidation(LoginModel login_cred)
+        {
+            _Connection.Open();
+            SqlCommand cmd = new SqlCommand("Retrive_Password", _Connection);
+            cmd.CommandType= CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("UserId", login_cred.Id);
+            cmd.Parameters.AddWithValue("@Email",login_cred.Email);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[0]);
+                string psw = (string)dr[0];
+                if(psw == login_cred.Password)
+                {
+                    return true;
+                }
+            }
+            _Connection.Close();
+
+            return false;
+        }
+        // POST: AdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel login)
+        {
+            try
+            {
+                if(login.UserName=="admin12345" && login.Password == "psw")
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                if (LoginValidation(login))
+                {
+                    Console.WriteLine(LoginValidation(login));
+                    return RedirectToAction("Index","User");
+                }
+                else
+                {
+                    Console.WriteLine(LoginValidation(login));
+                    var response = new
+                    {
+                        message = "Your alert message here"
+                    };
+                    Response.WriteAsJsonAsync(response);
+                    return RedirectToAction("Login", "Login");
+                }
+
             }
             catch
             {
--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,12 +1,21 @@
-﻿namespace AppointmentScheduler.Models
+﻿using Microsoft.Build.Framework;
+using System.Numerics;
+
+namespace AppointmentScheduler.Models
 {
     public class LoginModel
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Email { get; set; }
-        public double ContactNo { get; set; }
+        [Required]
+        public Int64 ContactNo { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Cmpy_name { get; set; }
         
 
